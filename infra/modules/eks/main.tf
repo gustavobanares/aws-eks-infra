7fa6eea5a50f914d828a78cc98f082c6a67965ee
Gustavo Bañares <gustavobbacco@gmail.com>
2026-04-02 19:13:29 -0300

    feat: implement modular EKS infrastructure with VPC networking and initial Kubernetes deployment manifests

---
+++ infra/modules/eks/main.tf
@@ -0,0 +1,28 @@
+module "eks" {
+    source  = "terraform-aws-modules/eks/aws"
+    version = "~> 20.0"
+
+    cluster_name                   = "tptech-cluster"
+    cluster_version                = "1.32"
+    cluster_endpoint_public_access = true
+    enable_cluster_creator_admin_permissions = true
+
+    eks_managed_node_groups = {
+      default = {
+        min_size       = 1
+        max_size       = 3
+        desired_size   = 2
+        instance_types = ["t3.medium"]
+
+        use_custom_launch_template = false
+      }
+    }
+
+    vpc_id     = var.vpc_id
+    subnet_ids = var.subnet_ids
+
+    tags = {
+      Environment = "dev"
+      Terraform   = "true"
+    }
+}
